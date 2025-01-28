c96dc30e04f5ab9b87469d33ad13e29751747384
Ana Lauren Dourado <analaurendourado@gmail.com>
2025-01-28 16:00:14 -0300

    revisão banco de dados e php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+include('conexao.php');  
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];  
+    $senha = $_POST['senha'];  
+
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        if ($senha == $row['senha']) {
+            header("Location: index.html");
+            exit;
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logint</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body class='login'>
+    <header>
+        <h1>Descubra como foram as minhas férias de verão! 🎀</h1>
+        <p>By Ana Lauren</p>
+    </header>
+    <br><br><br>
+    <div class="container" style="width: 300px;">
+        <h2>Login</h2>
+        <form method="post" action="">
+            <label for="email">Email:</label>
+            <input type="email" name="email" required>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" required>
+            <button type="submit" style="margin-bottom:40px;">Entrar</button>
+        </form>
+    </div>
+</body>
+</html>
